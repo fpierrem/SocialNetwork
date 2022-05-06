e201e98bc936dd64b8476050f2162cf8d9f6c541
florian <florian@florians-mbp-2.home>
2022-05-06 15:03:46 +0200

    Update settings

--- project4/settings.py
+++ project4/settings.py
@@ -139,3 +139,7 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 MEDIA_URL = '/media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+# Configure Django App for Heroku.
+import django_heroku
+django_heroku.settings(locals())
