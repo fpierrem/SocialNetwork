5ced667406910867ce478b9e5870413e87a91388
florian <florian@florians-MacBook-Pro-2.local>
2022-05-10 18:01:04 +0200

    Update settings

--- network/urls.py
+++ network/urls.py
@@ -1,6 +1,7 @@
-from django.urls import path
+from django.urls import path, re_path
 from django.conf.urls.static import static
 from django.conf import settings
+from django.views.static import serve
 
 from . import views
 
@@ -27,5 +28,7 @@ urlpatterns = [
     path("unlike", views.unlike, name="unlike"),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += re_path(r'^media/(?P<path>.*)$',serve,{'document_root':settings.MEDIA_ROOT})
+
+# if settings.DEBUG:
+#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
