bbe1420b3a6dd9daf53984d196996fac63601964
florian <florian@florians-MacBook-Pro-2.local>
2022-05-10 19:03:05 +0200

    Update settings

--- project4/settings.py
+++ project4/settings.py
@@ -20,8 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = os.environ['SECRET_KEY']
-SECRET_KEY = "JgWHwXX0tS/h9gKRbkn+a+UKY96wpp4Utm9e0w/hCV8="
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
