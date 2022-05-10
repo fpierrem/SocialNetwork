a14a3d765d203c7a69e1b48e77dfc22f87fc7b4a
florian <florian@florians-MacBook-Pro-2.local>
2022-05-10 16:51:48 +0200

    Update settings

--- network/test.py
+++
@@ -1,6 +0,0 @@
-from models import User, Post, Comment, Like, Follow
-
-test_post = Post.objects.get(id = 16)
-comments = test_post.post_comments
-
-print(comments)
--- project4/settings.py
+++ project4/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = "JgWHwXX0tS/h9gKRbkn+a+UKY96wpp4Utm9e0w/hCV8="
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
 
