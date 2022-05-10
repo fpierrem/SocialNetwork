66c354ceecb0f4d807c16019bf0edd62366a6919
florian <florian@florians-MacBook-Pro-2.local>
2022-05-10 18:03:09 +0200

    Update settings

--- project4/settings.py
+++ project4/settings.py
@@ -130,9 +130,9 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATIC_URL = '/static/'
 
-# STATICFILES_DIRS = (
-#     os.path.join(BASE_DIR, 'static'),
-# )
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 WHITENOISE_USE_FINDERS = True
 
